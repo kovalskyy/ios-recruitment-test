94cd2b53e699b813938a8dfa6ac263b65019e3fa
Kacper Kowalski <kowal@Mac-mini-Kacper.local>
2017-02-02 22:42:58 +0100

    searchBar

--- iOSRecruitmentTest/ViewController.swift
+++ iOSRecruitmentTest/ViewController.swift
@@ -16,32 +16,34 @@ class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelega
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    var inSearchMode = false
-    var filetered = [ItemRealm]()
-    
     let realm = try! Realm()
     var results = try! Realm().objects(ItemRealm.self).sorted(byKeyPath: "id")
     
-    //MARK: - Pull to refresh
+    private let SERVER_URL = "http://192.168.1.101:8080/api/items"
     
+    var inSearchMode = false
+    var filetered = [ItemRealm]()
+    
+    
+    //MARK: - Pull to refresh
     lazy var refreshControl: UIRefreshControl = {
+        
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(handleRefresh), for: UIControlEvents.valueChanged)
         return refreshControl
+        
     }()
     
     func handleRefresh() {
-        ServerService.sharedInstance.downloadAndSaveItem()
+        downloadAndSaveItem()
         self.refreshControl.endRefreshing()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ServerService.sharedInstance.downloadAndSaveItem()
-        self.tableView.reloadData()
-        
-
+        downloadAndSaveItem()
+    
         
         // Do any additional setup after loading the view.
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
@@ -51,6 +53,9 @@ class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelega
 
     // MARK: - UITableView data source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if inSearchMode {
+            return self.filetered.count
+        }
         return results.count
     }
     
@@ -58,15 +63,17 @@ class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelega
         let cell =  tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
         
         var object: ItemRealm
-        object = self.results[indexPath.row] as ItemRealm
+        if (inSearchMode && filetered.count > 0) {
+            object = self.filetered[indexPath.row] as ItemRealm
+        } else {
+            object = self.results[indexPath.row] as ItemRealm
+        }
         
         cell.item = object
-        
         return cell
     }
     
-    //MARK: - UISeachBar 
-    
+    //MARK: - UISeachBar
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchBar.showsCancelButton = true
         inSearchMode = true
@@ -86,9 +93,32 @@ class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelega
             tableView.reloadData()
         } else {
             inSearchMode = true
+            let lower = searchBar.text?.capitalized
+            
+            filetered = results.filter({$0.name.range(of: lower!) != nil})
+            tableView.reloadData()
         }
     }
     
-    
-    
+    //MARK: - Network Connection
+    func downloadAndSaveItem() {
+        
+        Alamofire.request(SERVER_URL).responseJSON { response in
+            let items = [Item].from(jsonArray: response.result.value as! [Gloss.JSON])
+            
+            try! self.realm.write {
+                for item in items! {
+                    let itemRealm = ItemRealm()
+                    itemRealm.id = item.id!
+                    itemRealm.name = item.name!
+                    itemRealm.desc = item.descr!
+                    itemRealm.icon = item.icon!
+                    self.realm.add(itemRealm, update: true)
+                    
+                }
+            }
+            
+            self.tableView.reloadData()
+        }
+    }
 }
